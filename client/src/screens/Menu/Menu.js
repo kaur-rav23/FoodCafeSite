83aa9f1c61ecec0b3ae10ad9b99ad540cdce09cd
Ravleen Kaur <102215556+kaur-rav23@users.noreply.github.com>
2024-08-07 00:29:00 +0530

    Update Menu.js

--- client/src/screens/Menu/Menu.js
+++ client/src/screens/Menu/Menu.js
@@ -8,7 +8,7 @@ export default function Menu() {
     const [foodItem, setFoodItem] = useState([]);
 
     const loadData = async () => {
-        let response = await fetch("http://localhost:5000/api/foodData", {
+        let response = await fetch("https://gofoods-zomd.onrender.com/api/foodData", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
