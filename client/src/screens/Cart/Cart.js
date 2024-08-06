ea48ffc5e3f3ec035a8254dd0ec2b21a348c4b2b
Ravleen Kaur <102215556+kaur-rav23@users.noreply.github.com>
2024-08-07 00:24:38 +0530

    Update Cart.js

--- client/src/screens/Cart/Cart.js
+++ client/src/screens/Cart/Cart.js
@@ -32,7 +32,7 @@ export default function Cart() {
 
     const handleCheckOut = async () => {
         let userEmail = localStorage.getItem("userEmail");
-        let response = await fetch("http://localhost:5000/api/orderData", {
+        let response = await fetch("https://gofoods-zomd.onrender.com/api/orderData", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
