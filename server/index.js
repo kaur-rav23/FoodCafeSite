d0c4c74f70725f4849989fb3d4331aea25b2e783
Ravleen Kaur <102215556+kaur-rav23@users.noreply.github.com>
2024-08-07 00:06:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,6 +25,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
